30ed4d80be9e7de537ea153751509b29a230af9c
AH-Sazzad <mdasaduzzamanasad100@gmail.com>
2026-02-08 00:23:53 +0600

    second task complete

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -11,9 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
+                
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('mobile')->unique()->nullable();
+            $table->string('otp')->default(0);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
